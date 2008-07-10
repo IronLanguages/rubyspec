12308400e957cc118c3e2dd8518226dbec2470ae
Jim Deville <jdeville@microsoft.com>
2008-07-10 15:06:21 -0700

    modified integer specs to add new tests

--- 1.8/core/integer/downto_spec.rb
+++ 1.8/core/integer/downto_spec.rb
@@ -19,6 +19,18 @@ describe "Integer#downto [stop] when self and stop are Fixnums" do
     result.should == [5, 4, 3, 2]
   end
 
+  it "yields while decreaing self until it less than ceil for a Float endpoint" do
+    result = []
+    9.downto(1.3) {|i| result << i}
+    result.should == [9, 8, 7, 6, 5, 4, 3, 2]
+  end
+
+  it "raises a ArgumentError for invalid endpoints" do
+    lambda {1.downto("A") {|x| p x } }.should raise_error(ArgumentError)
+    lambda {1.downto(nil) {|x| p x } }.should raise_error(ArgumentError)
+  end
+
+
   ruby_version_is "" ... "1.8.7" do
     it "raises a LocalJumpError when no block given" do
       lambda { 5.downto(2) }.should raise_error(LocalJumpError)
