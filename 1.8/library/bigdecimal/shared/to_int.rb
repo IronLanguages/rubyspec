9c5f430ac813e00fc980a4f201e38f0bda41568b
Pete Bacon Darwin <pete@bacondarwin.com>
2008-07-21 21:38:16 +0100

    Added negative values to check that to_i is rounding correctly

--- 1.8/library/bigdecimal/shared/to_int.rb
+++ 1.8/library/bigdecimal/shared/to_int.rb
@@ -15,6 +15,11 @@ shared :bigdecimal_to_int  do |cmd|
       BigDecimal("2").send(cmd).should == 2
       BigDecimal("2E10").send(cmd).should == 20000000000
       BigDecimal("3.14159").send(cmd).should == 3
+      BigDecimal("-3E-20001").send(cmd).should == 0
+      BigDecimal("-2E4000").send(cmd).should == -2 * 10 ** 4000
+      BigDecimal("-2").send(cmd).should == -2
+      BigDecimal("-2E10").send(cmd).should == -20000000000
+      BigDecimal("-3.14159").send(cmd).should == -3
     end
   end
 
